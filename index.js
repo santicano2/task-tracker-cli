32cc8aab22926e3e027d60b86ff58d470e4255ea
Santi <canosantiago404@gmail.com>
2024-08-13 12:34:48 -0300

    añadido welcome

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 
 console.log(chalk.bgGreen("Hola mama"));
+
+let userName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Bienvenido a Task Tracker CLI \n"
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`${chalk.bgBlue(
+    "- Añade, actualiza o borrar tareas"
+  )} \n${chalk.bgMagenta(
+    "- Muestra tareas pendientes o completadas"
+  )}\n${chalk.bgGreen(
+    "- Marca una tarea como pendiente o una tarea como completada"
+  )}
+		`);
+}
+
+await welcome();
